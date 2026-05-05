f87fcdd73ded96b33e37dd766cbb55109c104413
João Marcos <joaompscc@gmail.com>
2026-05-05 18:17:37 -0300

    Update ServiceCard.jsx

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,9 +1,36 @@
 import Link from "next/link";
 
+import {
+  FaHeart,
+  FaCar,
+  FaBuilding,
+  FaBriefcaseMedical,
+} from "react-icons/fa";
+
 export default function ServiceCard({ title, description, items }) {
+  const getIcon = () => {
+    if (title.includes("Vida"))
+      return <FaHeart className="service-icon" />;
+
+    if (title.includes("Auto"))
+      return <FaCar className="service-icon" />;
+
+    if (title.includes("Empresarial"))
+      return <FaBuilding className="service-icon" />;
+
+    if (title.includes("Saúde"))
+      return <FaBriefcaseMedical className="service-icon" />;
+
+    return null;
+  };
+
   return (
     <div className="card service-card">
-      <h3>{title}</h3>
+      <div className="service-top">
+        {getIcon()}
+        <h3>{title}</h3>
+      </div>
+
       <p>{description}</p>
 
       {items && (
